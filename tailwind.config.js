ec7a3e4f372b2c47e05a87056d83da5551fff74b
shanthalaksmik <santhalakshmi0423@gmail.com>
2024-12-24 12:07:43 +0530

    Initial commit for Real Estate App

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
