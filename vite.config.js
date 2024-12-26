0da8caf1958de8522a43d26ef452be23c4495357
shanthalaksmik <santhalakshmi0423@gmail.com>
2024-12-26 20:06:12 +0530

    Fixed base option and successful build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './Real Estate/',
+  base: './',
+  build: {
+    outDir: 'build', // Ensure the output directory is 'build'
+  },
 });
