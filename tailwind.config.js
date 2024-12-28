97b3738efb539fff08f49acb4fe7f2e62d2d5daa
shanthalaksmik <santhalakshmi0423@gmail.com>
2024-12-28 12:55:05 +0530

    Update project with latest changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
